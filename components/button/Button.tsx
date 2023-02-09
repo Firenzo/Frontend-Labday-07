4e96601fbfd039d022b4dadb47f543e89704eba3
Isabelle Leemhuis <99618812+IsabelleLeemhuis@users.noreply.github.com>
2023-02-09 16:02:57 +0100

    pdf

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -11,10 +11,10 @@ export function Button() {
 
     return (
         <div className={styles.Button}>
-            <a href="../../public/cv.pdf" target="_blank">
+            <a href="https://github.com/Firenzo/Frontend-Labday-07/raw/master/public/cv.pdf" target="_blank">
                 <Image src={dutchFlag} alt="Dutch version" width={50} />
             </a>
-            <a href="../../public/cv.pdf" target="_blank">
+            <a href="https://github.com/Firenzo/Frontend-Labday-07/raw/master/public/cv.pdf" target="_blank">
                 <Image src={britishFlag} alt="English version" width={50} />    
             </a>
         </div>
